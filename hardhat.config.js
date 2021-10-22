4272e0e99916c2f86494e2e497c6e3b92cc6b95c
Dan Octavian <danoctavian91@gmail.com>
2021-10-23 02:59:30 +0300

    add hh config stuff

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,38 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const networks = {
+  hardhat: {
+    accounts: {
+      count: 100,
+      accountsBalance: ether(1000000000),
+    },
+    hardfork: 'berlin',
+    allowUnlimitedContractSize: true,
+    blockGasLimit: 12e6,
+    gas: 12e6,
+  },
+  localhost: {
+    blockGasLimit: 12e6,
+    gas: 12e6,
+  },
+};
+
+const getenv = (network, key, fallback, parser = i => i) => {
+  const value = process.env[`${network}_${key}`];
+  return value ? parser(value) : fallback;
+};
+
+for (const network of ['MAINNET', 'KOVAN', 'MUMBAI']) {
+  const url = getenv(network, 'PROVIDER_URL', false);
+  if (!url) continue;
+  const accounts = getenv(network, 'ACCOUNT_KEY', undefined, v => v.split(/[^0-9a-fx]+/i));
+  const gasPrice = getenv(network, 'GAS_PRICE', undefined, v => parseInt(v, 10) * 1e9);
+  const gasLimit = getenv(network, 'GAS_LIMIT', undefined, v => parseInt(v, 10));
+  networks[network.toLowerCase()] = { accounts, gasPrice, gasLimit, url };
+}
+
 module.exports = {
   solidity: "0.8.4",
 };
