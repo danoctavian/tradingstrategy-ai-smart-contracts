9703dc2e470163e7eda8805107132a709c8781f2
Dan Octavian <danoctavian91@gmail.com>
2021-10-23 16:17:20 +0300

    move scripts + hardcode matic addresses

---
+++ scripts/common.js
@@ -0,0 +1,11 @@
+
+
+const Addresses = {
+  QUICKSWAP: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff',
+  USDC: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+  WMATIC: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+}
+
+module.exports = {
+  Addresses
+}
