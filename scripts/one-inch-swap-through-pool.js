05dd2c426c1d59d2b100a5aa08554aeac0182e02
Dan Octavian <danoctavian91@gmail.com>
2021-10-23 22:11:19 +0300

    add script for one-inch-swap-through-pool

---
+++ scripts/one-inch-swap-through-pool.js
@@ -0,0 +1,60 @@
+const { artifacts, config, network, run, web3 } = require('hardhat')
+const { ether} = require('@openzeppelin/test-helpers')
+const { Addresses } = require('./common')
+const fetch = require('node-fetch')
+
+
+const Pool = artifacts.require('Pool')
+const IAaveLendingPool = artifacts.require('IAaveLendingPool')
+const IERC20Detailed = artifacts.require('IERC20Detailed')
+const fs = require('fs')
+
+// inspired by https://github.com/smye/1inch-swap/blob/master/scripts/swap.py
+
+async function main() {
+
+
+  const contracts = JSON.parse(fs.readFileSync('deployment.json', 'utf8'))
+  const chainId = 137
+  const amount = ether('0.0001')
+  const sender = contracts.pool
+  const slippage = 50; // not sure
+
+  const fromTokenAddress = Addresses.DAI
+  const toTokenAddress = Addresses.USDC
+  const oneInchBuilderURL =
+    `https://api.1inch.exchange/v3.0/${chainId}/swap?fromTokenAddress=${fromTokenAddress}&toTokenAddress=${toTokenAddress}&` +
+    `amount=${amount.toString()}&fromAddress=${sender}&slippage=${slippage.toString()}&destReceiver=${sender}&disableEstimate=true`
+
+  console.log({
+    oneInchBuilderURL
+  })
+
+  const r = await fetch(oneInchBuilderURL).then(r => r.json())
+  const data = r.tx.data
+
+  console.log({
+    data
+  })
+
+
+  // const router = await IAaveLendingPool.at(Addresses.ONE_INCH_ROUTER)
+  //
+  // const fromToken = await IERC20Detailed.at(fromTokenAddress)
+
+
+  const pool = await Pool.at(contracts.pool);
+  console.log('swap...')
+  await pool.swapTokensOn1Inch(data, '0', Addresses.ONE_INCH_ROUTER)
+
+  //   await lendingPool.call(data)
+}
+
+
+
+main().catch(error => {
+  console.error('An unexpected error encountered:', error)
+  process.exit(1)
+});
+
+
